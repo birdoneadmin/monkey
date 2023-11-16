b4d20311f4126e4cdd683896d65af93e14a6d7b5
Korn <79989518+kornbirdone@users.noreply.github.com>
2023-11-16 12:24:27 +0000

    Save for now

--- monkey.py
+++ monkey.py
@@ -23,3 +23,52 @@ def banana(action):
     b = 0
   else:
     ooaa("Error 1: Invalid Banana Function")
+
+# Loops
+def monkey(command, subcommand, times):
+  if command == "ooaa":
+    for i in range(times):
+      ooaa(subcommand)
+  elif command == "banana":
+    for i in range(times):
+      banana(subcommand)
+  else:
+    ooaa("Error 2: Invalid Monkey Function Provided")
+
+# Concatenation
+def double(text):
+  if isinstance(text, str):
+    return text + text
+  else:
+    return str(text) + str(text)
+
+# Math
+def math(n1, func, n2):
+  if func == "add" or func == "+":
+    return n1 + n2
+  elif func == "subtract" or func == "-":
+    return n1 - n2
+  elif func == "multiply" or func == "*":
+    return n1 * n2
+  elif func == "divide" or func == "/":
+    return n1 / n2
+  else:
+    ooaa("Error 3: Invalid Math Function")
+
+# If-else
+def ifelse(value, condition, ifcmd, ifsub, elsecmd, elsesub)
+  if condition == "=" or condition == "==" or condition == "equal":
+    if b == value:
+      if ifcmd == "ooaa":
+        ooaa(ifsub)
+      elif ifcmd == "banana":
+        banana(ifsub)
+      else:
+        ooaa("Error 4: Invalid Ifelse Function Provided")
+    else:
+      if elsecmd == "ooaa":
+        ooaa(elsesub)
+      elif elsecmd == "banana":
+        banana(elsesub)
+      else:
+        ooaa("Error 4: Invalid Ifelse Function Provided")
