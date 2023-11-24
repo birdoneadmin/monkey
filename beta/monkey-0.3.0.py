46bff1e2a2f3f046e3a685d036957c4ca7a0b27e
Korn <79989518+kornbirdone@users.noreply.github.com>
2023-11-24 13:18:23 +0000

    MONKEY 0.3.0

--- beta/monkey-0.3.0.py
+++ beta/monkey-0.3.0.py
@@ -1,6 +1,6 @@
 #########################################
 ###### MONKEY PROGRAMMING LANGUAGE ######
-############ Version 0.2.1 ##############
+############ Version 0.3.0 ##############
 #########################################
 
 # ----------------------------------------------------- #
@@ -9,19 +9,15 @@
 b = 0
 pext = 0
 pextext = 0
+printok = False
 import random
+import sys
+from time import sleep
 
 # ----------------------------------------------------- #
 
-# Block Outside Commands
-def mkyprint(*args, **kwargs):
-    if allow_print:
-        print(*args, **kwargs)
-    else:
-        raise ValueError("Error 6: Invalid Function. Use ooaa() or oohoohahah() instead.")
-
-print = mkyprint
-
+# Modifications
+    
 def getb():
     return b
 
@@ -35,7 +31,69 @@ def EnablePrintExt():
 def DisablePrintExt():
     global pext
     pext = 0
-
+    
+def PrintExt():
+    print("")
+    sleep(0.5)
+    print(".----. .----. -.- |\\   | --.--")
+    print("|    | |    |  |  | \\  |   |  ")
+    print("|----. |----.  |  |  \\ |   |  ")
+    print("|      |   \\   |  |   \\|   |  ")
+    print("|      |    \\ _._ |    |   |  ")
+    print("")
+    sleep(0.2)
+    print("|---- \\   / --.-- |---- |\\   |")
+    print("|      \\ /    |   |     | \\  |")
+    print("|----   X     |   |---- |  \\ |")
+    print("|      / \\    |   |     |   \\|")
+    print("|---- /   \\   |   |---- |    |")
+    print("")
+    sleep(0.2)
+    print("PrintExten")
+    print("Official Print Extension for Monkey 0.3.0")
+    print("")
+    print("Choose what you want to do:")
+    print("1. Enable Print Exten")
+    print("2. Disable Print Exten")
+    print("3. Leave a Review")
+    print("4. Exit")
+    print("")
+    pxchoice = input("... ")
+    
+    if pxchoice == "1":
+        EnablePrintExt()
+        return
+    elif pxchoice == "2":
+        DisablePrintExt()
+        return
+    elif pxchoice == "3":
+        pxreview = input("\nType your review... (No more than 128 characters)\n... ")
+        if len(pxreview) > 128:
+            print("\nPxError 1: Review too long! Not saved.")
+            print("")
+            for i in range(3):
+                print("Going to Home Screen in "+str(3-i)+" second(s)...")
+                sleep(1)
+            PrintExt()
+        elif pxreview == "":
+            print("\nRating Left Blank. Going Back to Home Screen")
+            sleep(1)
+            PrintExt()
+        else:
+            pxrating = input("\nThank you. Please rate us!\n_/5 (0-5) ... ")
+            print(pxrating+"/5 Thank you!") if (isinstance(pxrating, int) and pxrating >= 0 and pxrating <= 5) else (print("\nPxError 2: Invalid Rating! Not saved."))
+            print("")
+            for i in range(3):
+                print("Going to Home Screen in "+str(3-i)+" second(s)...")
+                sleep(1)
+            PrintExt()
+    elif pxchoice == "4":
+        pxconf = input("Confirm Exit? (y/n)")
+        if pxconf == "y":
+            return
+        else:
+            PrintExt()
+    
 # ----------------------------------------------------- #
 
 # Print Commands
@@ -50,10 +108,13 @@ def oohoohahah(text):
     else:
         print("Ooh Ooh Ah Ah")
         if text != "":
-            ooaaa("Error 8: Enable Print Exten first to output text.")
+            pextext = 1
+            sleep(0.5)
+            print("Error 8: Enable Print Exten first to output text.")
+            pextext = 0
     allow_print = False
 
-def ooaaa(text):
+def ooaa(text):
     oohoohahah(text)
 
 def ooaaa(text):
@@ -77,7 +138,7 @@ def banana(action):
         b = 0
     else:
         pextext = 1
-        ooaaa("Error 1: Invalid Banana Function")
+        print("Error 1: Invalid Banana Function")
         pextext = 0
 
 # ----------------------------------------------------- #
@@ -87,13 +148,13 @@ def monkey(command, subcommand, times):
     global pextext
     if command == "ooaa":
         for i in range(times):
-            ooaaa(subcommand)
+            ooaa(subcommand)
     elif command == "banana":
         for i in range(times):
             banana(subcommand)
     else:
         pextext = 1
-        ooaaa("Error 2: Invalid Monkey Loop Function Provided")
+        ooaa("Error 2: Invalid Monkey Loop Function Provided")
         pextext = 0
 
 # ----------------------------------------------------- #
@@ -120,7 +181,7 @@ def math(n1, func, n2):
         return n1 / n2
     else:
         pextext = 1
-        ooaaa("Error 3: Invalid Math Function")
+        ooaa("Error 3: Invalid Math Function")
         pextext = 0
 
 # ----------------------------------------------------- #
@@ -131,18 +192,18 @@ def ifelse(value, condition, ifcmd, ifsub, elsecmd, elsesub):
     if condition == "=" or condition == "==" or condition == "equal":
         if b == value:
             if ifcmd == "ooaa":
-                ooaaa(ifsub)
+                ooaa(ifsub)
             elif ifcmd == "banana":
                 banana(ifsub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
         else:
             if elsecmd == "ooaa":
-                ooaaa(elsesub)
+                ooaa(elsesub)
             elif elsecmd == "banana":
                 banana(elsesub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
     elif (
         condition == ">"
         or condition == "greater"
@@ -151,18 +212,18 @@ def ifelse(value, condition, ifcmd, ifsub, elsecmd, elsesub):
     ):
         if b > value:
             if ifcmd == "ooaa":
-                ooaaa(ifsub)
+                ooaa(ifsub)
             elif ifcmd == "banana":
                 banana(ifsub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
         else:
             if elsecmd == "ooaa":
-                ooaaa(elsesub)
+                ooaa(elsesub)
             elif elsecmd == "banana":
                 banana(elsesub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
     elif (
         condition == "<"
         or condition == "less"
@@ -171,20 +232,20 @@ def ifelse(value, condition, ifcmd, ifsub, elsecmd, elsesub):
     ):
         if b < value:
             if ifcmd == "ooaa":
-                ooaaa(ifsub)
+                ooaa(ifsub)
             elif ifcmd == "banana":
                 banana(ifsub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
         else:
             if elsecmd == "ooaa":
-                ooaaa(elsesub)
+                ooaa(elsesub)
             elif elsecmd == "banana":
                 banana(elsesub)
             else:
-                pextext=1; ooaaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
+                pextext=1; ooaa("Error 4: Invalid Ifelse Function Provided"); pextext=0
     else:
-        ooaaa("Error 5: Invalid Ifelse Condition Provided")
+        ooaa("Error 5: Invalid Ifelse Condition Provided")
 
 # ----------------------------------------------------- #
 
@@ -207,13 +268,13 @@ def comment():
 def who():
     global pextext
     pextext = 1
-    ooaaa("da")
+    ooaa("da")
     pextext = 0
 
 def whn(text):
     global pextext
     pextext = 1
-    ooaaa("da"+capitalize(text))
+    ooaa("da"+text.upper())
     pextext = 0
 
 # ----------------------------------------------------- #
